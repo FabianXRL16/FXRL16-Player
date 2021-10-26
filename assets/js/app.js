aa7f967fb4dafb5b9ea0a98de9a150a448a7d5a5
Fabian Pacherres <fabianpacherres@gmail.com>
2021-10-26 07:22:20 -0500

    Play and pause functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,3 +23,22 @@ function toShowInput() {
   // inputShow = !inputShow
   // input.value = ""
 }
+
+let $video = document.querySelector('video');
+let $play = document.querySelector('#play');
+let $pause = document.querySelector('#pause');
+
+$play.addEventListener('click', handlePlay)
+$pause.addEventListener('click', handlePause)
+
+function handlePlay(){
+  $video.play()
+  $play.hidden = true
+  $pause.hidden = false
+}
+
+function handlePause(){
+  $video.pause()
+  $pause.hidden = true
+  $play.hidden = false
+}
