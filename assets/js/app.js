82cdf747a2f1f713d6fb845ad593c10f84da60fe
Fabian Pacherres <fabianpacherres@gmail.com>
2021-10-29 00:12:47 -0500

    To show input dinamic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,54 @@
 let inputShow = true;
 
-function search(){
-    toShowInput()
+function search() {
+  toShowInput();
 }
 
 function toShowInput() {
-  //   let input = document.querySelector("#inputSearch");
-  //   let containerList = document.querySelector(".containerList");
-  //   let padList = containerList.style.paddingTop
-  // if (inputShow) {
-  //   input.style.transform = "translateY(40px)";
-  //   input.style.transition = ".3s";
-  //   containerList.style.paddingTop = `${padList+50}px`;
-  //   containerList.style.transition = ".3s";
-  // } else {
-  //   input.style.transform = "translateY(0)";
-  //   input.style.transition = ".3s";
-
-  //   containerList.style.paddingTop = `${padList+100}px`;
-  //   containerList.style.transition = ".3s";
-  // }
-  // inputShow = !inputShow
-  // input.value = ""
+  let input = document.querySelector("#inputSearch");
+  let containerList = document.querySelector(".containerList");
+  let pad = parseInt(containerList.style.paddingTop.replace("px", ""));
+  let widthScreen = screen.width
+  if (inputShow) {
+    input.style.transform = "translateY(40px)";
+    input.style.transition = ".3s";
+    if(widthScreen > 480 && widthScreen <= 768){
+      pad+=70
+    }else{
+      pad+= 50
+    }
+    containerList.style.paddingTop = pad + "px";
+    containerList.style.transition = ".3s";
+  } else {
+    input.style.transform = "translateY(0)";
+    if(widthScreen > 480 && widthScreen <= 768){
+      containerList.style.paddingTop = "65px";
+    }else{
+      containerList.style.paddingTop = "50px";
+    }
+    input.style.transition = ".3s";
+    containerList.style.transition = ".3s";
+  }
+  inputShow = !inputShow;
+  input.value = "";
+  console.log(containerList.style.paddingTop);
 }
 
-let $video = document.querySelector('video');
-let $play = document.querySelector('#play');
-let $pause = document.querySelector('#pause');
+let $video = document.querySelector("video");
+let $play = document.querySelector("#play");
+let $pause = document.querySelector("#pause");
 
-$play.addEventListener('click', handlePlay)
-$pause.addEventListener('click', handlePause)
+$play.addEventListener("click", handlePlay);
+$pause.addEventListener("click", handlePause);
 
-function handlePlay(){
-  $video.play()
-  $play.hidden = true
-  $pause.hidden = false
+function handlePlay() {
+  $video.play();
+  $play.hidden = true;
+  $pause.hidden = false;
 }
 
-function handlePause(){
-  $video.pause()
-  $pause.hidden = true
-  $play.hidden = false
+function handlePause() {
+  $video.pause();
+  $pause.hidden = true;
+  $play.hidden = false;
 }
