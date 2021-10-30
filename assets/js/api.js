d6dba212eeb916d5c4d5ec3f5e6ce97347540854
Fabian Pacherres <fabianpacherres@gmail.com>
2021-10-30 17:52:32 -0500

    Export async function init

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,18 +1,15 @@
+const KEY = "AIzaSyAlTAWqXWSEkSrKP6lrV2y07DGyx3Vtug8";
+const API = "https://youtube.googleapis.com/youtube/v3/search";
+const PARAMS = "&type=video&part=snippet&maxResults=";
+const MAX_RESULTS = 3;
 
-const KEY = 'AIzaSyAlTAWqXWSEkSrKP6lrV2y07DGyx3Vtug8';
-const API = 'https://youtube.googleapis.com/youtube/v3/search';
-const PARAMS = '&type=video&part=snippet&maxResults=';
-const MAX_RESULTS = 3
-
-let items = []
-
-const init = async (url) => {
-    try {
-      const data = await (await fetch(url)).json();
-      items = data.items
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  
-  init(`${API}?key=${KEY}${PARAMS}${MAX_RESULTS}&q=fxrl16`);
+export default async function init(findMusic = "fxrl16") {
+  try {
+    let url = `${API}?key=${KEY}${PARAMS}${MAX_RESULTS}&q=${findMusic}`;
+    const data = await (await fetch(url)).json();
+    let items = data.items;
+    return items;
+  } catch (error) {
+    console.error(error);
+  }
+}
