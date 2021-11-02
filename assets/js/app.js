c164674dbba9f543aa66fc8e24e4ba5d2bdc2bd0
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-02 00:46:46 -0500

    Items of playList is dinamic

--- assets/js/app.js
+++ assets/js/app.js
@@ -83,4 +83,59 @@ function handlePause() {
   $play.hidden = false;
 }
 
-let playList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+let playList = [
+  {
+    id: 1,
+    img: "https://direct.rhapsody.com/imageserver/images/alb.61525160/600x600.jpg",
+    title: "One More Night",
+    singer: "Maroon 5"
+  },
+
+  {
+    id: 2,
+    img: "https://static.qobuz.com/images/covers/ra/gu/w4jwafymtgura_600.jpg",
+    title: "Stay",
+    singer: "Justin Bieber & The Kid Laroi"
+  },
+  {
+    id: 3,
+    img: "https://los40es00.epimg.net/los40/imagenes/2019/02/28/musica/1551370175_698722_1551370391_noticia_normal.jpg",
+    title: "Sucker",
+    singer: "Jonas Brothers"
+  },
+  {
+    id: 4,
+    img: "https://lastfm.freetls.fastly.net/i/u/300x300/c63984cd7a6f404bc0f4e1c3bc17e0c7.jpg",
+    title: "Hasta la manyana",
+    singer: "Elefante"
+  }
+]
+
+function showPlayList(){
+  let $container = document.querySelector('.containerList')
+  playList.map(item => {
+    let $btn = document.createElement("BUTTON")
+    $btn.classList.add('itemList')
+    let $img = document.createElement("DIV")
+    $img.classList.add('img')
+    $img.style.backgroundImage = `url(${item.img})`
+    let $title = document.createElement("DIV")
+    $title.classList.add('title')
+    let $h2 = document.createElement("H2")
+    $h2.classList.add('titleSong')
+    let $p = document.createElement("P")
+    $p.classList.add("singer")
+    let nameSong = document.createTextNode(item.title)
+    let singer = document.createTextNode(item.singer)
+    $h2.appendChild(nameSong)
+    $p.appendChild(singer)
+    $title.appendChild($h2)
+    $title.appendChild($p)
+    $btn.appendChild($img)
+    $btn.appendChild($title)
+    $container.appendChild($btn)
+  })
+
+}
+
+showPlayList()
